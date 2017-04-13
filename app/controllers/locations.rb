7788355d818e35fc9ec256a240f0933840daef8d
Elsie Koglin <email": null>
2017-04-13 16:37:27 -0500

    Added Dark Sky

--- app/controllers/locations.rb
+++ app/controllers/locations.rb
@@ -1,15 +1,15 @@
-
 get '/locations' do
-  # location = Location.find_by(city: params[:city])
-  erb :'locations/index'
+  # darksky = ENV['DARK_SKY_API_KEY']
+
+  # erb :'locations/index', locals: {darksky_key: darksky }
 end
 
 get '/locations/:id' do
-  @my_api_key = ENV['GOOGLE_API_KEY']
-  # p my_api_key
+  google = ENV['GOOGLE_API_KEY']
+  darksky = ENV['DARK_SKY_API_KEY']
   # @location = Location.find_by(id: params[:id])
   # @lat = @location.lat
   # @lng = @location.lng
-  erb :'locations/show'
+  erb :'locations/show', locals: {google_key: google, darksky_key: darksky }
 end
 
