35fdff4fabf24b78a83d0a688f610190f29d87ab
Elsie Koglin <email": null>
2017-04-13 15:32:37 -0500

    dotenv and key working

--- app/controllers/locations.rb
+++ app/controllers/locations.rb
@@ -1,9 +1,12 @@
+
 get '/locations' do
   # location = Location.find_by(city: params[:city])
   erb :'locations/index'
 end
 
 get '/locations/:id' do
+  @my_api_key = ENV['GOOGLE_API_KEY']
+  # p my_api_key
   # @location = Location.find_by(id: params[:id])
   # @lat = @location.lat
   # @lng = @location.lng
