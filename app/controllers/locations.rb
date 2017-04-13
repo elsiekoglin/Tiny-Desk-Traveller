96a4bd97c8ad29ec48f22d3723d400d8af0ce912
Elsie Koglin <email": null>
2017-04-13 17:34:04 -0500

    Working on Dark Sky

--- app/controllers/locations.rb
+++ app/controllers/locations.rb
@@ -1,15 +1,20 @@
-get '/locations' do
-  # darksky = ENV['DARK_SKY_API_KEY']
+require 'net/http'
 
-  # erb :'locations/index', locals: {darksky_key: darksky }
+get '/locations' do
+
+  erb :'locations/index'
 end
 
 get '/locations/:id' do
   google = ENV['GOOGLE_API_KEY']
   darksky = ENV['DARK_SKY_API_KEY']
+  old_uri = URI("https://api.darksky.net/forecast/#{darksky}/42.345573,-71.098326")
+# old_uri = URI("https://darksky.net/dev/docs/forecast")
+
+  uri = Net::HTTP.get(old_uri)
   # @location = Location.find_by(id: params[:id])
   # @lat = @location.lat
   # @lng = @location.lng
-  erb :'locations/show', locals: {google_key: google, darksky_key: darksky }
+  erb :'locations/show', locals: {google_key: google, link: uri }
 end
 
