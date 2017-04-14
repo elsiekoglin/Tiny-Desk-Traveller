53886c0b439e9b36002bfb5305408d73a58c773d
Elsie Koglin <email": null>
2017-04-14 14:40:01 -0500

    Done....

--- app/controllers/locations.rb
+++ app/controllers/locations.rb
@@ -8,10 +8,9 @@ end
 get '/locations/:id' do
   google = ENV['GOOGLE_API_KEY']
   darksky = ENV['DARK_SKY_API_KEY']
-  old_uri = URI("https://api.darksky.net/forecast/#{darksky}/42.345573,-71.098326")
-# old_uri = URI("https://darksky.net/dev/docs/forecast")
+  # old_uri = URI("https://api.darksky.net/forecast/#{darksky}/-33.8688,151.2195")
 
-  uri = Net::HTTP.get(old_uri)
+  # uri = Net::HTTP.get(old_uri)
   # @location = Location.find_by(id: params[:id])
   # @lat = @location.lat
   # @lng = @location.lng
