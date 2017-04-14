eed33ea67a1196f50e1e8d9b88ab882ed770b681
Elsie Koglin <email": null>
2017-04-14 15:07:05 -0500

    Just need to add css

--- app/controllers/locations.rb
+++ app/controllers/locations.rb
@@ -5,17 +5,18 @@ get '/locations' do
   erb :'locations/index'
 end
 
-get '/locations/:id' do
+get '/locations/cities' do
   google = ENV['GOOGLE_API_KEY']
-  darksky = ENV['DARK_SKY_API_KEY']
-  # old_uri = URI("https://api.darksky.net/forecast/#{darksky}/-33.8688,151.2195")
 
-  # uri = Net::HTTP.get(old_uri)
-  # @location = Location.find_by(id: params[:id])
-  # @lat = @location.lat
-  # @lng = @location.lng
-  erb :'locations/show', locals: {google_key: google, link: uri }
+
+  erb :'locations/show', locals: {google_key: google }
 end
 
-# <%= link %>
 
+get '/locations/:id' do
+  google = ENV['GOOGLE_API_KEY']
+  @location = Location.find_by(id: params[:id])
+  @lat = @location.lat
+  @lng = @location.lng
+  erb :'locations/built_in', locals: {google_key: google }
+end
